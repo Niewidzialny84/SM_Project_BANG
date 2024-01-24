b05217955ba31715fed0fbfb9daeb4f4e340f155
CatOnXTC <karolsienkiewicz03@gmail.com>
2024-01-24 21:18:55 +0100

    Request User and Room name
    Added in addition GUI for displaying error messages. I'll just add it to main to not create next branch to instantly get pr

--- Assets/Scripts/MenuManagerUI.cs
+++ Assets/Scripts/MenuManagerUI.cs
@@ -2,23 +2,55 @@ using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManagerUI : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Button createRoomButton;
     [SerializeField] private Button joinRoomButton;
-    [SerializeField] private TextMeshProUGUI roomCodeTextbox;
+    [SerializeField] private GameObject roomCodeTextbox;
     [SerializeField] private string sceneName;
+    private GameObject errorText;
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(roomCodeTextbox.text, new RoomOptions() {MaxPlayers = 2, IsVisible = true, IsOpen = true}, TypedLobby.Default, null);
+        if (roomCodeTextbox.GetComponent<TMP_InputField>().text != "")
+        {
+            //Debug.Log(roomCodeTextbox.GetComponent<TMP_InputField>().text);
+            errorText = GameObject.FindGameObjectWithTag("ErrorMSG");
+            if (errorText is not null)
+            {
+                errorText.GetComponent<TMP_Text>().text = "";
+            }
+            PhotonNetwork.CreateRoom(roomCodeTextbox.GetComponent<TMP_InputField>().text, new RoomOptions() { MaxPlayers = 2, IsVisible = true, IsOpen = true }, TypedLobby.Default, null);
+        }
+        else
+        {
+            Debug.Log("ERROR: No Room name!");
+            errorText = GameObject.FindGameObjectWithTag("ErrorMSG");
+            if (errorText is not null)
+            {
+                errorText.GetComponent<TMP_Text>().text = "Missing room code";
+            }
+        }
     }
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom(roomCodeTextbox.text);
+        if (roomCodeTextbox.GetComponent<TMP_InputField>().text != "")
+        {
+            PhotonNetwork.JoinRoom(roomCodeTextbox.GetComponent<TMP_InputField>().text);
+        }
+        else
+        {
+            Debug.Log("ERROR: No Room name!");
+            errorText = GameObject.FindGameObjectWithTag("ErrorMSG");
+            if (errorText is not null)
+            {
+                errorText.GetComponent<TMP_Text>().text = "Missing room code";
+            }
+        }    
     }
 
     public override void OnJoinedRoom()
--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -1,11 +1,52 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScreenManager : MonoBehaviour
 {
+    private GameObject inputText;
+    private GameObject errorText;
+    [SerializeField] protected string username;
+
     public void ChangeScreen(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        switch (sceneName)
+        {
+            case "Loading Screen":
+                inputText = GameObject.FindGameObjectWithTag("Username");
+
+                if (inputText is null)
+                {
+                    Debug.Log("ERROR: Object not found!");
+                }
+                //Debug.Log(inputText.GetComponent<TMP_InputField>().text);
+                else if (inputText.GetComponent<TMP_InputField>().text != "")
+                {
+                    errorText = GameObject.FindGameObjectWithTag("ErrorMSG");
+                    if (errorText is not null)
+                    {
+                        errorText.GetComponent<TMP_Text>().text = "";
+                    }
+                    username = inputText.GetComponent<TMP_InputField>().text;
+                    SceneManager.LoadScene(sceneName);
+                    break;
+                }
+                else
+                { 
+                    Debug.Log("ERROR: No Username!");
+                    errorText = GameObject.FindGameObjectWithTag("ErrorMSG");
+                    if(errorText is not null)
+                    {
+                        errorText.GetComponent<TMP_Text>().text = "Missing username";
+                    }
+                }
+                
+                break;
+            default:
+                // code block
+                break;
+        }
     }
 
     private void Awake()
